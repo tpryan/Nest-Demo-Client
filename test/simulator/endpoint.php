f987399264d8cb181f2c9804b64e16a6872db757
Terrence Ryan <terry@terrenceryan.com>
2014-09-18 00:19:15 -0400

    Working on a mock endpoint for unit testing.

---
+++ test/simulator/endpoint.php
@@ -0,0 +1,31 @@
+<?php
+	$content = file_get_contents("sample.json");
+	$data = json_decode($content);
+
+
+
+	$path = array_slice(explode("/", $_GET['path']),1);
+
+	if ( strlen(end($path)) == 0 ){
+		array_pop($path);
+	}
+
+	if (count($path) == 0 ){
+		$result  = $content;
+	} else if (count($path) == 1 ){
+		$result  = $data->$path[0];
+	} else if (count($path) == 2 ){
+		$result  = $data->$path[0]->$path[1];
+	} else if (count($path) == 3 ){
+		$result  = $data->$path[0]->$path[1]->$path[2];
+	} else if (count($path) == 4 ){
+		$result  = $data->$path[0]->$path[1]->$path[2]->$path[3];
+	} 
+
+
+
+
+
+	echo(json_encode($result));
+
+?>
